dc7b030d8f46989819c9642f740f7dfb2c4e87e2
Andres-Martinez-Gutierrez <95919805+Andres-Martinez-Gutierrez@users.noreply.github.com>
2023-04-15 06:32:18 -0500

    Navigation bottom

--- app/src/main/java/com/example/horoscopoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/MainActivity.kt
@@ -1,11 +1,29 @@
-package com.example.horoscopoapp
+package com.example.horoscopoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscopoapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var  navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavView.setupWithNavController(navController)
+
     }
+
+
+
 }
